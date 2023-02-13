ebc745137f412ac67c2d4226c31a5c0060b8e564
afanaa <90841532+afanaa@users.noreply.github.com>
2023-02-13 11:02:33 +0800

    Checked with TA

--- src/lab1/HelloWorld.java
+++ src/lab1/HelloWorld.java
@@ -2,6 +2,6 @@ package lab1;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-        System.out.print("This is a demo project.");
+        System.out.print("Hello World!");
     }
 }
